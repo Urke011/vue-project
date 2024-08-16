32e1358023168f8fdc2eac990252c4579fc63710
Uros Rajkovic <urosrajkovic011@hotmail.com>
2024-08-16 19:55:37 +0200

    set project

---
+++ src/App2.vue
@@ -0,0 +1,46 @@
+<script>
+  export default{
+    data(){
+        return{
+          name: 'John Doe',
+          status: 'pending',
+          tasks: [
+            'Task one', 'Task two', 'Task three'
+          ],
+          link: 'https://www.google.com'
+        }; 
+    },
+    methods:{
+      toggleStatus(){
+        if(this.status === 'active'){
+          this.status = 'pending';
+        }else if(this.status === 'pending'){
+          this.status = 'inactive';
+        }else{
+          this.status = 'active';
+        }
+      }
+    }
+  };
+</script>
+
+<template>
+  <h2>{{ name }}</h2>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else="status">User is inactive</p>
+
+  <h3>Tasks: </h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <div>
+    <a v-bind:href="link">Click for Google</a>
+  </div>
+  <div>
+    <a :href="link">Click for Google</a>
+  </div>
+  <br>
+  <button @click="toggleStatus">Change status</button>
+</template>
+
